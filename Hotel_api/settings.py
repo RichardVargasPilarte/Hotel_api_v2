293547b621e92d2e83be3ef916b638ae19c6400b
RichardVargasPilarte <ricardo.pilarte94@mail.com>
2022-09-12 08:56:10 -0600

    correcion de static

--- Hotel_api/settings.py
+++ Hotel_api/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
