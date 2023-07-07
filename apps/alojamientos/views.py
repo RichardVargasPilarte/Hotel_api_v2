928dc7aeb58f4ef4e0bf9d5cb3dda24f1b6c1a81
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2023-07-07 15:44:18 -0600

    correccion en la implementacion del enum en las  clases listdo y detalle alojamientos

--- apps/alojamientos/views.py
+++ apps/alojamientos/views.py
@@ -21,56 +21,56 @@ class ListadoAlojamiento(APIView, ClassQuery):
 
     def get(self, request):
         try:
-            status_code = 200
-            response = HTTPResponse.get_message(status_code)
+            # status_code = 200
+            # response = HTTPResponse.get_message(status_code)
             alojamientos = Alojamiento.objects.filter(
                 eliminado="NO").order_by('id')
             serializer = alojamientoSerializer(alojamientos, many=True)
-            return Response(dict(data=serializer.data, code=status_code))
+            return Response(dict(data=serializer.data, code=HTTPResponse.OK))
         except:
-            status_code = 404
+            # status_code = 404
             response = 'Registros no encontrados'  # Default custom message for other errors
 
             # Handle specific codes differently
             if specific_condition_1:
-                status_code = 400
+                status_code = HTTPResponse.BAD_REQUEST
                 response = HTTPResponse.get_message(status_code)
             elif specific_condition_2:
-                status_code = 401
+                status_code = HTTPResponse.UNAUTHORIZED
                 response = HTTPResponse.get_message(status_code)
             else:
-                status_code = 500
+                status_code = HTTPResponse.INTERNAL_SERVER_ERROR
                 response = HTTPResponse.get_message(status_code)
 
-            return Response(dict(data=[], detail=response, code=status_code))
+            return Response(dict(data=[], detail=response, code=HTTPResponse.NOT_FOUND))
             # return Response(dict(data=[], detail="not found", code=status_code))
 
     def post(self, request):
         try:
-            status_code = 201
-            response = HTTPResponse.get_message(status_code)
+            # status_code = 201
+            # response = HTTPResponse.get_message(status_code)
             alojamiento = request.data.get('alojamiento')
             print(alojamiento)
             serializer = alojamientoSerializer(data=alojamiento)
             if serializer.is_valid(raise_exception=True):
                 alojamiento_saved = serializer.save()
-            return Response(dict(message=f"Alojamiento: '{alojamiento_saved.nombre}' creada satisfactoriamente".format(), code=status_code))
+            return Response(dict(message=f"Alojamiento: '{alojamiento_saved.nombre}' creada satisfactoriamente".format(), code=HTTPResponse.CREATED))
         except:
-            status_code = 404
+            # status_code = 404
             response = 'Registro no creado'
             
             # Handle specific codes differently
             if specific_condition_1:
-                status_code = 400
+                status_code = HTTPResponse.BAD_REQUEST
                 response = HTTPResponse.get_message(status_code)
             elif specific_condition_2:
-                status_code = 401
+                status_code = HTTPResponse.UNAUTHORIZED
                 response = HTTPResponse.get_message(status_code)
             else:
-                status_code = 500
+                status_code = HTTPResponse.INTERNAL_SERVER_ERROR
                 response = HTTPResponse.get_message(status_code)
 
-            return Response(dict(data=[], detail=response, code=status_code))
+            return Response(dict(data=[], detail=response, code=HTTPResponse.NOT_FOUND))
 
 
 class DetalleAlojamiento(APIView, ClassQuery):
@@ -83,33 +83,32 @@ class DetalleAlojamiento(APIView, ClassQuery):
 
     def get(self, request, pk):
         try:
-            status_code = 200
-            response = HTTPResponse.get_message(status_code)
+            # status_code = 200
+            # response = HTTPResponse.get_message(status_code)
             alojamientos = Alojamiento.objects.get(id=pk)
             serializer = alojamientoSerializer(alojamientos)
-            return Response(dict(alojamientos=serializer.data, code=status_code))
+            return Response(dict(alojamientos=serializer.data, code=HTTPResponse.OK))
         except:
-            status_code = 404
+            # status_code = 404
             response = 'Registro no encontrado'
             
             # Handle specific codes differently
             if specific_condition_1:
-                status_code = 400
+                status_code = HTTPResponse.BAD_REQUEST
                 response = HTTPResponse.get_message(status_code)
             elif specific_condition_2:
-                status_code = 401
+                status_code = HTTPResponse.UNAUTHORIZED
                 response = HTTPResponse.get_message(status_code)
             else:
-                status_code = 500
+                status_code = HTTPResponse.INTERNAL_SERVER_ERROR
                 response = HTTPResponse.get_message(status_code)
 
-            return Response(dict(data=[], detail=response, code=status_code))
-            # return Response(dict(alojamientos=[], detail="not found", code=404))
+            return Response(dict(data=[], detail=response, code=HTTPResponse.NOT_FOUND))
 
     def put(self, request, pk):
         try:
-            status_code = 200
-            response = HTTPResponse.get_message(status_code)
+            # status_code = 200
+            # response = HTTPResponse.get_message(status_code)
             saved_alojamientos = get_object_or_404(
                 Alojamiento.objects.all(), id=pk)
             alojamientos = request.data.get('alojamientos')
@@ -118,47 +117,47 @@ class DetalleAlojamiento(APIView, ClassQuery):
                 instance=saved_alojamientos, data=alojamientos, partial=True)
             if serializer.is_valid(raise_exception=True):
                 alojamiento_saved = serializer.save()
-            return Response(dict(message=f'Alojamiento [{alojamiento_saved.nombre}] actualizado correctamente', code=status_code))
+            return Response(dict(message=f'Alojamiento [{alojamiento_saved.nombre}] actualizado correctamente', code=HTTPResponse.OK))
     
         except:
-            status_code = 404
+            # status_code = 404
             response = 'Hubo un error al actializar el registro'
             
             # Handle specific codes differently
             if specific_condition_1:
-                status_code = 400
+                status_code = HTTPResponse.BAD_REQUEST
                 response = HTTPResponse.get_message(status_code)
             elif specific_condition_2:
-                status_code = 401
+                status_code = HTTPResponse.UNAUTHORIZED
                 response = HTTPResponse.get_message(status_code)
             else:
-                status_code = 500
+                status_code = HTTPResponse.INTERNAL_SERVER_ERROR
                 response = HTTPResponse.get_message(status_code)
 
-            return Response(dict(data=[], detail=response, code=status_code))
-
+            return Response(dict(data=[], detail=response, code=HTTPResponse.NOT_FOUND))
 
     def delete(self, request, pk):
         try:
-            status_code = 204
+            # status_code = 204
             response = HTTPResponse.get_message(status_code)
             alojamientos = get_object_or_404(Alojamiento.objects.all(), id=pk)
             alojamientos.eliminado = 'SI'
             alojamiento_saved = alojamientos.save()
-            return Response(dict(message=f'Alojamiento con id `[{pk}]` fue eliminado.'), status=204)
+            return Response(dict(message=f'Alojamiento con id `[{pk}]` fue eliminado.'), status=HTTPResponse.NO_CONTENT)
             
         except:
-            status_code = 404
+            # status_code = 404
             response = 'Registro no eliminado'
 
+            # Handle specific codes differently
             if specific_condition_1:
-                status_code = 400
+                status_code = HTTPResponse.BAD_REQUEST
                 response = HTTPResponse.get_message(status_code)
             elif specific_condition_2:
-                status_code = 401
+                status_code = HTTPResponse.UNAUTHORIZED
                 response = HTTPResponse.get_message(status_code)
             else:
-                status_code = 500
+                status_code = HTTPResponse.INTERNAL_SERVER_ERROR
                 response = HTTPResponse.get_message(status_code)
-                
-            return Response(dict(data=[], detail=response, code=status_code))
+
+            return Response(dict(data=[], detail=response, code=HTTPResponse.NOT_FOUND))
