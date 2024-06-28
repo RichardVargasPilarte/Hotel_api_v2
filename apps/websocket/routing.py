3230388774c1b986f2f4bfafb1d35059df5238be
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-28 11:46:43 -0600

    Update WebSocket URL pattern to use secure 'wss' protocol

--- apps/websocket/routing.py
+++ apps/websocket/routing.py
@@ -4,5 +4,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/", consumers.Consumer.as_asgi()),
+    #re_path(r"ws/", consumers.Consumer.as_asgi()),
+    re_path(r"wss/", consumers.Consumer.as_asgi()),
 ]
