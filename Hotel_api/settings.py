6d7d34e63cc45a35141ac16afc6b353e82450fe3
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-28 18:13:15 -0600

    Fix environment variable usage for Redis host in CHANNEL_LAYERS configuration

--- Hotel_api/settings.py
+++ Hotel_api/settings.py
@@ -149,7 +149,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_HOST'), os.environ.get('REDIS_PORT'))],
+            "hosts": [(os.environ.get('REDIS_HOST'))],
         },
     },
 }
